ec13508de39f6b477bdf61b509dabdafc6790df7
fengzhenbing <fengzhenbing98@126.com>
2020-11-25 20:30:29 +0800

    week06

---
+++ Week_06/my_shop.sql
@@ -0,0 +1,234 @@
+/*
+SQLyog Ultimate v12.5.0 (64 bit)
+MySQL - 5.7.31 : Database - my_shop
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`my_shop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
+
+USE `my_shop`;
+
+/*Table structure for table `member` */
+
+DROP TABLE IF EXISTS `member`;
+
+CREATE TABLE `member` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `member_level_id` bigint(20) DEFAULT NULL,
+  `username` varchar(64) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(64) DEFAULT NULL COMMENT '密码',
+  `nickname` varchar(64) DEFAULT NULL COMMENT '昵称',
+  `phone` varchar(64) DEFAULT NULL COMMENT '手机号码',
+  `status` int(1) DEFAULT NULL COMMENT '帐号启用状态:0->禁用；1->启用',
+  `create_time` datetime DEFAULT NULL COMMENT '注册时间',
+  `icon` varchar(500) DEFAULT NULL COMMENT '头像',
+  `gender` int(1) DEFAULT NULL COMMENT '性别：0->未知；1->男；2->女',
+  `birthday` date DEFAULT NULL COMMENT '生日',
+  `city` varchar(64) DEFAULT NULL COMMENT '城市',
+  `job` varchar(100) DEFAULT NULL COMMENT '职业',
+  `personalized_signature` varchar(200) DEFAULT NULL COMMENT '个性签名',
+  `source_type` int(1) DEFAULT NULL COMMENT '用户来源 1 小程序 2 公众号 3 页面',
+  `avatar` varchar(255) DEFAULT NULL,
+  `weixin_openid` varchar(50) DEFAULT NULL,
+  `invitecode` varchar(20) DEFAULT NULL,
+  `blance` decimal(10,0) DEFAULT NULL COMMENT '余额',
+  `school_id` bigint(20) DEFAULT NULL,
+  `area_id` bigint(20) DEFAULT NULL,
+  `school_name` varchar(255) DEFAULT NULL,
+  `area_name` varchar(255) DEFAULT NULL,
+  `buy_count` int(11) DEFAULT NULL,
+  `buy_money` decimal(10,0) DEFAULT NULL,
+  `member_level_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='会员表';
+
+/*Table structure for table `order` */
+
+DROP TABLE IF EXISTS `order`;
+
+CREATE TABLE `order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单id',
+  `member_id` bigint(20) NOT NULL,
+  `order_sn` varchar(64) DEFAULT NULL COMMENT '订单编号',
+  `create_time` datetime DEFAULT NULL COMMENT '提交时间',
+  `member_username` varchar(512) DEFAULT NULL COMMENT '用户帐号',
+  `total_amount` decimal(10,2) DEFAULT NULL COMMENT '订单总金额',
+  `pay_amount` decimal(10,2) DEFAULT '0.00' COMMENT '应付金额（实际支付金额）',
+  `freight_amount` decimal(10,2) DEFAULT NULL COMMENT '运费金额',
+  `pay_type` int(1) DEFAULT NULL COMMENT '支付方式：0->未支付；1->支付宝；2->微信',
+  `source_type` int(1) DEFAULT NULL COMMENT '订单来源：0->PC订单；1->app订单',
+  `status` int(1) DEFAULT NULL COMMENT '订单状态：0->待付款；1->待发货；2->已发货；3->已完成；4->已关闭；5->无效订单',
+  `order_type` int(1) DEFAULT NULL COMMENT '订单类型：0->正常订单；1->秒杀订单',
+  `auto_confirm_day` int(11) DEFAULT NULL COMMENT '自动确认时间（天）',
+  `note` varchar(500) DEFAULT NULL COMMENT '订单备注',
+  `confirm_status` int(1) DEFAULT NULL COMMENT '确认收货状态：0->未确认；1->已确认',
+  `delete_status` int(1) NOT NULL DEFAULT '0' COMMENT '删除状态：0->未删除；1->已删除',
+  `use_integration` int(11) DEFAULT NULL COMMENT '下单时使用的积分',
+  `payment_time` datetime DEFAULT NULL COMMENT '支付时间',
+  `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1050 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='订单表';
+
+/*Table structure for table `order_item` */
+
+DROP TABLE IF EXISTS `order_item`;
+
+CREATE TABLE `order_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(20) DEFAULT NULL COMMENT '订单id',
+  `order_sn` varchar(64) DEFAULT NULL COMMENT '订单编号',
+  `product_id` bigint(20) DEFAULT NULL,
+  `product_pic` varchar(500) DEFAULT NULL,
+  `product_name` varchar(200) DEFAULT NULL,
+  `product_brand` varchar(200) DEFAULT NULL,
+  `product_sn` varchar(64) DEFAULT NULL,
+  `product_price` decimal(10,2) DEFAULT NULL COMMENT '销售价格',
+  `product_quantity` int(11) DEFAULT NULL COMMENT '购买数量',
+  `product_sku_id` bigint(20) DEFAULT NULL COMMENT '商品sku编号',
+  `product_sku_code` varchar(50) DEFAULT NULL COMMENT '商品sku条码',
+  `product_category_id` bigint(20) DEFAULT NULL COMMENT '商品分类id',
+  `sp1` varchar(100) DEFAULT NULL COMMENT '商品的销售属性',
+  `sp2` varchar(100) DEFAULT NULL,
+  `sp3` varchar(100) DEFAULT NULL,
+  `product_attr` varchar(500) DEFAULT NULL COMMENT '商品销售属性:[{"key":"颜色","value":"颜色"},{"key":"容量","value":"4G"}]',
+  `status` int(4) DEFAULT NULL,
+  `type` int(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1191 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='订单中所包含的商品';
+
+/*Table structure for table `product` */
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `brand_id` bigint(20) DEFAULT NULL,
+  `product_category_id` bigint(20) DEFAULT NULL,
+  `feight_template_id` bigint(20) DEFAULT NULL,
+  `product_attribute_category_id` bigint(20) DEFAULT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `pic` varchar(255) DEFAULT NULL,
+  `product_sn` varchar(64) DEFAULT NULL COMMENT '货号',
+  `delete_status` int(1) DEFAULT NULL COMMENT '删除状态：0->未删除；1->已删除',
+  `publish_status` int(1) DEFAULT NULL COMMENT '上架状态：0->下架；1->上架',
+  `new_status` int(1) DEFAULT NULL COMMENT '新品状态:0->不是新品；1->新品',
+  `recommand_status` int(1) DEFAULT NULL COMMENT '推荐状态；0->不推荐；1->推荐',
+  `verify_status` int(1) DEFAULT NULL COMMENT '审核状态：0->未审核；1->审核通过',
+  `sort` int(11) DEFAULT NULL COMMENT '排序',
+  `sale` int(11) DEFAULT NULL COMMENT '销量',
+  `price` decimal(10,2) DEFAULT NULL,
+  `promotion_price` decimal(10,2) DEFAULT '0.00' COMMENT '促销价格',
+  `use_point_limit` int(11) DEFAULT NULL COMMENT '限制使用的积分数',
+  `sub_title` varchar(255) DEFAULT NULL COMMENT '副标题',
+  `description` text COMMENT '商品描述',
+  `original_price` decimal(10,2) DEFAULT NULL COMMENT '市场价',
+  `stock` int(11) DEFAULT NULL COMMENT '库存',
+  `low_stock` int(11) DEFAULT NULL COMMENT '库存预警值',
+  `unit` varchar(16) DEFAULT NULL COMMENT '单位',
+  `weight` decimal(10,2) DEFAULT NULL COMMENT '商品重量，默认为克',
+  `preview_status` int(1) DEFAULT NULL COMMENT '是否为预告商品：0->不是；1->是',
+  `service_ids` varchar(64) DEFAULT NULL COMMENT '以逗号分割的产品服务：1->无忧退货；2->快速退款；3->免费包邮',
+  `keywords` varchar(255) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `album_pics` varchar(255) DEFAULT NULL COMMENT '画册图片，连产品图片限制为5张，以逗号分割',
+  `detail_title` varchar(255) DEFAULT NULL,
+  `detail_desc` text,
+  `detail_html` text COMMENT '产品详情网页内容',
+  `detail_mobile_html` text COMMENT '移动端网页详情',
+  `promotion_start_time` datetime DEFAULT NULL COMMENT '促销开始时间',
+  `promotion_end_time` datetime DEFAULT NULL COMMENT '促销结束时间',
+  `promotion_per_limit` int(11) DEFAULT NULL COMMENT '活动限购数量',
+  `promotion_type` int(1) DEFAULT NULL COMMENT '促销类型：0->没有促销使用原价;1->使用促销价；2->使用会员价；3->使用阶梯价格；4->使用满减价格；5->限时购',
+  `brand_name` varchar(255) DEFAULT NULL COMMENT '品牌名称',
+  `product_category_name` varchar(255) DEFAULT NULL COMMENT '商品分类名称',
+  `supply_id` bigint(20) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `member_id` bigint(20) DEFAULT NULL,
+  `hit` int(11) DEFAULT '0',
+  `type` int(11) DEFAULT NULL,
+  `area_id` bigint(20) DEFAULT NULL,
+  `area_name` varchar(255) DEFAULT NULL,
+  `school_name` varchar(255) DEFAULT NULL,
+  `transfee` decimal(10,0) NOT NULL DEFAULT '0' COMMENT '运费',
+  `expire_time` datetime DEFAULT NULL COMMENT '到期时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10548 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='商品信息';
+
+/*Table structure for table `product_attribute` */
+
+DROP TABLE IF EXISTS `product_attribute`;
+
+CREATE TABLE `product_attribute` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_attribute_category_id` bigint(20) DEFAULT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `select_type` int(1) DEFAULT NULL COMMENT '属性选择类型：0->唯一；1->单选；2->多选',
+  `input_type` int(1) DEFAULT NULL COMMENT '属性录入方式：0->手工录入；1->从列表中选取',
+  `input_list` varchar(255) DEFAULT NULL COMMENT '可选值列表，以逗号隔开',
+  `sort` int(11) DEFAULT NULL COMMENT '排序字段：最高的可以单独上传图片',
+  `filter_type` int(1) DEFAULT NULL COMMENT '分类筛选样式：1->普通；1->颜色',
+  `search_type` int(1) DEFAULT NULL COMMENT '检索类型；0->不需要进行检索；1->关键字检索；2->范围检索',
+  `related_status` int(1) DEFAULT NULL COMMENT '相同属性产品是否关联；0->不关联；1->关联',
+  `hand_add_status` int(1) DEFAULT NULL COMMENT '是否支持手动新增；0->不支持；1->支持',
+  `type` int(1) DEFAULT NULL COMMENT '属性的类型；0->规格；1->参数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='商品属性参数表';
+
+/*Table structure for table `product_attribute_value` */
+
+DROP TABLE IF EXISTS `product_attribute_value`;
+
+CREATE TABLE `product_attribute_value` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) DEFAULT NULL,
+  `product_attribute_id` bigint(20) DEFAULT NULL,
+  `value` varchar(64) DEFAULT NULL COMMENT '手动添加规格或参数的值，参数单值，规格有多个时以逗号隔开',
+  `name` varchar(255) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=887 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='存储产品参数信息的表';
+
+/*Table structure for table `product_category` */
+
+DROP TABLE IF EXISTS `product_category`;
+
+CREATE TABLE `product_category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '上机分类的编号：0表示一级分类',
+  `name` varchar(64) DEFAULT NULL,
+  `level` int(1) DEFAULT NULL COMMENT '分类级别：0->1级；1->2级',
+  `product_count` int(11) DEFAULT NULL,
+  `product_unit` varchar(64) DEFAULT NULL,
+  `nav_status` int(1) DEFAULT NULL COMMENT '是否显示在导航栏：0->不显示；1->显示',
+  `show_status` int(1) DEFAULT NULL COMMENT '显示状态：0->不显示；1->显示',
+  `index_status` int(1) NOT NULL DEFAULT '0' COMMENT '是否是首页分类0-->不是，1-->是',
+  `sort` int(11) DEFAULT NULL,
+  `icon` varchar(255) DEFAULT NULL COMMENT '图标',
+  `keywords` varchar(255) DEFAULT NULL,
+  `description` text COMMENT '描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1440 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='产品分类';
+
+/*Table structure for table `product_category_attribute_relation` */
+
+DROP TABLE IF EXISTS `product_category_attribute_relation`;
+
+CREATE TABLE `product_category_attribute_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_category_id` bigint(20) DEFAULT NULL,
+  `product_attribute_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='产品的分类和属性的关系表，用于设置分类筛选条件（只支持一级分类）';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
